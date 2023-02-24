72c132e2495483ed40094e265bfd14e298311e2b
kanagatzhumagaliev <zhumagalievkanagat@gmail.com>
2023-02-24 18:42:57 +0600

    login page

---
+++ pages/login/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="d-flex flex-column page">
+    <NavBar/>
+    <div class="flex-shrink-1 main__container flex-grow-1">
+      <div class="px-4">
+        <div class="login__container">
+          <h1>Вход</h1>
+          <div class="error__message">
+            Логин или пароль введены неверно.
+            Пожалуйста, попробуйте еще раз
+          </div>
+          <v-form>
+            <v-text-field label="Логин"/>
+            <v-text-field label="Пароль"/>
+            <v-btn elevation="0">Пароль</v-btn>
+          </v-form>
+        </div>
+      </div>
+    </div>
+    <Footer/>
+  </div>
+</template>
+
+<script setup>
+import NavBar from '../../components/NavBar'
+import Footer from "../../components/Footer";
+</script>
