da98fe13555b30ae8228a49f0591c1fd4da6314d
kanagatzhumagaliev <zhumagalievkanagat@gmail.com>
2023-02-26 15:38:29 +0600

    navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="navbar__container">
+  <div class="navbar__container" :class="route.name === 'index' ? 'py-10' : 'py-6'">
     <div class="limit__container navbar__items d-flex align-center justify-space-between">
       <Icon class="pointer" icon-name="logo" @click="$router.push('/')"/>
       <ul class="navbar__items d-flex align-center">
-        <li>Как это работает</li>
-        <li>Для кого</li>
-        <li>Управление</li>
-        <li>Преимущества</li>
-        <li>Контакты</li>
+        <li v-if="route.name === 'index'">Как это работает</li>
+        <li v-if="route.name === 'index'">Для кого</li>
+        <li v-if="route.name === 'index'">Управление</li>
+        <li v-if="route.name === 'index'">Преимущества</li>
+        <li v-if="route.name === 'index'">Контакты</li>
         <li class="locale">Рус
           <icon class="ml-2" icon-name="arrow-down"/>
         </li>
         <li class="menu__toggle ml-7" @click="isMenuOpen = !isMenuOpen">
           <icon :icon-name="isMenuOpen ? 'close' : 'menu-toggle'"/>
+        </li>
+        <li v-if="route.name !== 'index'" class="restaurant__logo">
+          <img src="../assets/images/restaurant-logo.png" alt="">
+          <h3>Ресторан Дареджани</h3>
         </li>
       </ul>
     </div>
@@ -31,6 +35,8 @@
 <script setup>
 import Icon from "./icon";
 import {ref} from 'vue'
+import {useRoute} from "nuxt/app";
 
-const isMenuOpen = ref(false)
+const route = useRoute()
+let isMenuOpen = ref(false)
 </script>
